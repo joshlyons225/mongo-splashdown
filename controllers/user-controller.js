797ac8494c201d046d94cb8227d36d2555f6b2de
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-04-20 16:39:34 -0500

    initialize user-controller with export

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,2 +1,24 @@
 // import models
+const res = require("express/lib/response");
 const { User } = require("../models");
+
+// set User controller
+const userController = {
+  // get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
+
+// export user-controller
+module.exports = userController;
