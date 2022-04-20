642b3f4243ef696b58c01785886fdd6f02ab9d5c
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-04-20 16:11:09 -0500

    initial layout user-routes.js

---
+++ routes/api/user-routes.js
@@ -0,0 +1,12 @@
+// set dependencies
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller");
