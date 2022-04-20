d4cd3291b5079a0b725e0a8d0b735889d02c2e0b
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-04-20 16:45:12 -0500

    updated getUserById in user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,6 +18,25 @@ const userController = {
         res.status(400).json(err);
       });
   },
+
+  // get user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
 };
 
 // export user-controller
